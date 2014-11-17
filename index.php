b63610ac94cfca58203f46ae5ca940e1f9b7f2fd
Ella Durham <hello@elladurham.com>
2014-11-17 08:39:50 -0600

    Missing file in last commit.
    Unclicked the index.php for the last commit.

---
+++ index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+
+    <link href='http://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="fonts/foundation-icons/foundation-icons.css">
+    <link rel="stylesheet" href="css/proto-banao.css">
+
+  </head>
+  <body>
+    <?php include_once("img/svg-icons.svg"); ?>
+
+    <a href="#" class="button right">Get in Touch</a>
+    <div class="row">
+      <h1 class="text-center">
+        <img class="logo" src="img/proto-bano.svg">
+      </h1>
+    </div>
+    
+
+    <div class="project">
+      <div class="row">
+        <div class="large-3 medium-3 large-offset-1 columns">
+          <img class="vertical-center-later" src="img/project-sketch-name.svg">
+        </div>
+        <div class="large-2 large-offset-1 medium-2 medium-offset-1 columns">
+          <img class="vertical-center-later-also" src="img/arrow.svg">
+        </div>
+        <div class="large-3 medium-3 large-offset-1 columns end">
+          <img src="img/project-preview-name.svg">
+        </div>
+      </div>
+      <span class="text-content"><span>Project Name</span></span>
+    </div>
+
+
+    <div class="project">
+      <div class="row">
+        <div class="large-3 medium-3 large-offset-1 columns">
+          <img class="vertical-center-later" src="img/project-sketch-name.svg">
+        </div>
+        <div class="large-2 large-offset-1 medium-2 medium-offset-1 columns">
+          <img class="vertical-center-later-also" src="img/arrow.svg">
+        </div>
+        <div class="large-3 medium-3 large-offset-1 columns end">
+          <img src="img/project-preview-name.svg">
+        </div>
+      </div>
+      <span class="text-content"><span>Project Name</span></span>
+    </div>
+
+    <div class="project">
+      <div class="row">
+        <div class="large-3 medium-3 large-offset-1 columns">
+          <img class="vertical-center-later" src="img/project-sketch-name.svg">
+        </div>
+        <div class="large-2 large-offset-1 medium-2 medium-offset-1 columns">
+          <img class="vertical-center-later-also" src="img/arrow.svg">
+        </div>
+        <div class="large-3 medium-3 large-offset-1 columns end">
+          <img src="img/project-preview-name.svg">
+        </div>
+      </div>
+      <span class="text-content"><span>Project Name</span></span>
+    </div>
+
+    <div class="project">
+      <div class="row">
+        <div class="large-3 medium-3 large-offset-1 columns">
+          <img class="vertical-center-later" src="img/project-sketch-name.svg">
+        </div>
+        <div class="large-2 large-offset-1 medium-2 medium-offset-1 columns">
+          <img class="vertical-center-later-also" src="img/arrow.svg">
+        </div>
+        <div class="large-3 medium-3 large-offset-1 columns end">
+          <img src="img/project-preview-name.svg">
+        </div>
+      </div>
+      <span class="text-content"><span>Project Name</span></span>
+    </div>
+
+
+    <form>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>
+            <input type="text" placeholder="Email Address" />
+          </label>
+        </div>
+        <div class="row">
+          <div class="large-12 columns">
+            <label>
+              <textarea placeholder="small-12.columns"></textarea>
+            </label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="large-12 columns">
+            <label>
+              <input type="submit" value="send">
+            </label>
+          </div>
+        </div>
+      </div>
+    </form>
+    
+
+    <footer class="text-center">
+      <svg class="icon-social"><use xlink:href="#icon-facebook" /></svg>
+      <svg class="icon-social"><use xlink:href="#icon-twitter" /></svg>
+      <svg class="icon-social"><use xlink:href="#icon-linkedin" /></svg>
+      <svg class="icon-social"><use xlink:href="#icon-rss" /></svg>
+    </footer>
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>

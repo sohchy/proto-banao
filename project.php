534daf0cc261a208be0abefb1926b28043f29c14
Ella Durham <hello@elladurham.com>
2014-11-25 00:20:58 -0600

    Customizing Project Slider, Project Page
    Small styling for project page, began initial groundwork for
    customizing slick slider for the project.

--- project.php
+++ project.php
@@ -10,266 +10,167 @@
     <link href='http://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="css/proto-banao.css"/>
 
+
   </head>
   <body>
-  <?php include_once("img/svg-icons.svg"); ?>
+    <?php include_once("img/svg-icons.svg"); ?>
     <nav class="clearfix">
       <h1 class="left">Project Title</h1>
-      <a href="index.php#main-content" class="button right">Other Projects</a>
-      <a href="#" class="button right">Get in Touch</a>
+      <a href="index.php#main-content" class="button right show-for-large-up">Other Projects</a>
+      <a href="index.php#get-in-touch" class="button right show-for-large-up">Get in Touch</a>
     </nav>
 
-    <nav class="clearfix">
-      <svg class="left icon"><use xlink:href="#icon-back" /></svg>
-      <div class="row left text-center">
-        <div class="large-12 columns">
-          <dl class="tabs" data-tab>
-            <dd class="active">
-              <a href="#discovery">
-                <svg class="icon"><use xlink:href="#icon-discovery" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#research">
-                <svg class="icon"><use xlink:href="#icon-research" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#information">
-                <svg class="icon"><use xlink:href="#icon-information-architecture" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#messaging">
-                <svg class="icon"><use xlink:href="#icon-messaging" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#sketch">
-                <svg class="icon"><use xlink:href="#icon-sketch" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#mock-up">
-                <svg class="icon"><use xlink:href="#icon-mock-up" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#code">
-                <svg class="icon"><use xlink:href="#icon-code" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#enhance">
-                <svg class="icon"><use xlink:href="#icon-enhance" /></svg>
-              </a>
-            </dd>
-            <dd>
-              <a href="#user-testing">
-                <svg class="icon"><use xlink:href="#icon-user-testing" /></svg>
-              </a>
-            </dd>
-          </dl>
-        </div>
-      </div>
-      <svg class="icon right"><use xlink:href="#icon-next" /></svg>
-    </nav>
-
-    <div class="row">
-
-      <div class="large-12 columns">
-
-        <div class="tabs-content">
-        <div class="content active" id="discovery">
-          <div class="row">
-            <div class=" large-6 columns">
-              <h2>discovery</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        <div class="content" id="research">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>research</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        <div class="content" id="information">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>information</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-
-        <div class="content" id="messaging">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>messaging</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        <div class="content" id="sketch">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>sketch</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        <div class="content" id="mock-up">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>mock-up</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        <div class="content" id="code">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>code</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        <div class="content" id="enhance">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>enhance</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        <div class="content" id="user-testing">
-          <div class="row">
-            <div class="large-6 columns">
-              <h2>user testing</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="large-6 columns">
-              <div class="your-class">
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-                <div><img src="http://placehold.it/600x600"></div>
-              </div> 
-            </div>
-          </div>
-        </div>
-        </div>
-
-      </div>
-
+    <div class="slider slider-nav">
+      <div><svg class="icon"><use xlink:href="#icon-discovery" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-research" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-information-architecture" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-messaging" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-sketch" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-mock-up" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-code" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-enhance" /></svg></div>
+      <div><svg class="icon"><use xlink:href="#icon-user-testing" /></svg></div>
     </div>
-    
+
+  <div class="slider slider-for">
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Discovery</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/700x600">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Research</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/570x600">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Informatoin Architecture</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/550x575">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Messaging</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/600x675">
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Sketch</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/600x610">
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Mock-up</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/500x300">
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Code</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/600x300">
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>Enhance</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/700x700">
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="row">
+        <div class="large-6 columns">
+          <h2>User Testing</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+        <div class="large-6 columns">
+          <img data-lazy="http://placehold.it/650x650">
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="small-6 columns">
+    <a href="index.php#main-content" class="button hide-for-large-up">Other Projects</a>
+  </div>
+  <div class="small-6 columns">
+    <a href="index.php#get-in-touch" class="button hide-for-large-up">Get in Touch</a>
+  </div>
+      
 
     <footer class="text-center">
-      <svg class="icon-social"><use xlink:href="#icon-facebook" /></svg>
-      <svg class="icon-social"><use xlink:href="#icon-twitter" /></svg>
-      <svg class="icon-social"><use xlink:href="#icon-linkedin" /></svg>
-      <svg class="icon-social"><use xlink:href="#icon-rss" /></svg>
+      <a><svg class="icon-social"><use xlink:href="#icon-facebook" /></svg></a>
+      <a><svg class="icon-social"><use xlink:href="#icon-twitter" /></svg></a>
+      <a><svg class="icon-social"><use xlink:href="#icon-linkedin" /></svg></a>
+      <a><svg class="icon-social"><use xlink:href="#icon-rss" /></svg></a>
     </footer>
 
     
@@ -289,15 +190,31 @@
       });
     </script>
     <script>
-      $(document).ready(function(){
-        $('.your-class').slick({
-          arrows: false,
-          dots: true
+      $(document).ready(function () {
+        $('.slider-nav').slick({
+            centerMode: true,
+            infinite: false,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for',
+            focusOnSelect: true,
+            variableWidth: true
         });
+
+        $('.slider-for').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          fade: true,
+          asNavFor: '.slider-nav'
+        });
+        
+      });   
+      $('.lazy').slick({
+        lazyLoad: 'progressive',
+        slidesToShow: 1,
+        slidesToScroll: 1
       });
     </script>
-
-
 
   </body>
 </html>
